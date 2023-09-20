ebd84ada5c8cfe526e23a7c7c07e75b3779a12d0
GHAFHA <45609663+GHAFHA@users.noreply.github.com>
2023-09-20 01:06:17 -0500

    removed else block, no need

--- DAQ.py
+++ DAQ.py
@@ -25,7 +25,7 @@ class DAQObject:
         self.writer = csv.writer(self.file)
         self.ecu_columns = can_messages_cols
         self.ecu_df = pd.Dataframe(self.ecu_columns)
-        self.ECUData = [None] * 16
+        self.ECUData = [None] * 16 # why 16 ?
         self.LJData = []
         self.writeData = [None]
         self.handle = ljm.openS("T7")
@@ -61,8 +61,7 @@ class DAQObject:
                 self.daq_run_lock.acquire()
                 msg = bus.recv()
                 self.ecu_df.loc[self.ecu_df.index, "time"] = msg.timestamp
-                self.ecu_df.loc[self.ecu_df.index,
-                                str(msg.arbitration_id)] = msg.data
+                self.ecu_df.loc[self.ecu_df.index, str(msg.arbitration_id)] = msg.data
                 self.daq_run_lock.release()
 
     def resolveError(self) -> bool:
@@ -79,39 +78,37 @@ class DAQObject:
 
         nextTime = time.time()
 
-        while True:
+        while True: # while what is true ?
             self.can_read_lock.acquire()
+            
             if time.time() < nextTime:
                 time.sleep(0)
 
-            else:
-                nextTime = time.time()
+            if self.currentState == DAQState.INIT:
+                self.setSMState(DAQState.COLLECTING)
 
-                if self.currentState == DAQState.INIT:
-                    self.setSMState(DAQState.COLLECTING)
+            if self.currentState == DAQState.COLLECTING:
 
-                if self.currentState == DAQState.COLLECTING:
+                try:
+                    print(self.readLJ())
+                except ljm.LJMError:
+                    self.currentState == DAQState.ERROR
 
-                    try:
-                        print(self.readLJ())
-                    except ljm.LJMError:
-                        self.currentState == DAQState.ERROR
+                    if self.resolveError():
+                        break
 
-                        if self.resolveError():
-                            break
+                    print("LabJack Error", ljm.LJMError)
+                    self.write_zero_row()
 
-                        print("LabJack Error", ljm.LJMError)
-                        self.write_zero_row()
+                self.ecu_df.to_csv(self.output_file, index=False)
 
-                    self.ecu_df.to_csv(self.output_file, index=False)
+                # recordedTime = time.time()
+                # self.writeData.append(time.time())
+                # self.writeData.extend(self.ECUData)
+                # self.writer.writerow(self.writeData)
+                # self.writeData.clear()
 
-                    # recordedTime = time.time()
-                    # self.writeData.append(time.time())
-                    # self.writeData.extend(self.ECUData)
-                    # self.writer.writerow(self.writeData)
-                    # self.writeData.clear()
-
-            self.can_read_lock.release()
+        self.can_read_lock.release()
 
     def __del__(self):
 
