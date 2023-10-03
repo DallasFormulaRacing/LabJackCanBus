007a2c1fac83f8eeff01c15c4c720d51a32cbb3c
GHAFHA <45609663+GHAFHA@users.noreply.github.com>
2023-10-03 12:15:51 -0500

    added analog input for xl

---
+++ read_xl_analog.py
@@ -0,0 +1,18 @@
+# AN13 is x
+# AN12 is y
+# AN11 is z
+from labjack import ljm
+
+
+class read_xl_analog:
+  
+  def read_xl(handle):
+    x_axis = "AIN13"
+    y_axis = "AIN12"
+    z_axis = "AIN11"
+
+    result_x = ljm.eReadName(handle, x_axis)
+    result_y = ljm.eReadName(handle, y_axis)
+    result_z = ljm.eReadName(handle, z_axis)
+
+    print(f"\n{x_axis} reading : {result_x} V \n{y_axis} reading : {result_y} V \n{z_axis} reading : {result_z} V")
