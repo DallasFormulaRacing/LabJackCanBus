3702d3bc2670b997cba3667fe8f8fe92c4e179b0
NicoOhR <nimoohayonr@gmail.com>
2024-01-07 19:05:00 -0600

    I2C configuration

--- accel.py
+++ accel.py
@@ -15,32 +15,26 @@ handle = ljm.openS("ANY","ANY","ANY")
 
 info = ljm.getHandleInfo(handle)
 
-if info[0] == ljm.constants.dtT4:
-    ljm.eWriteName(handle, "DIO_INHIBIT", 0xFFF8F)
-    ljm.eWriteName(handle, "DIO_ANALOG_ENABLE", 0x00000)
+ljm.eWriteName(handle, "I2C_SDA_DIONUM", 1) #using FIO1 as SDA
+ljm.eWriteName(handle, "I2C_SCL_DIONUM", 0) #using FIO0 as SCL
 
-    ljm.eWriteName(handle, "I2C_SDA_DIONUM", 5)
-    ljm.eWriteName(handle, "I2C_SCL_DIONUM", 4)
-    ljm.eWriteName(handle, "FIO6", 1)
-
-else:
-
-    ljm.eWriteName(handle, "I2C_SDA_DIONUM", 1)
-    ljm.eWriteName(handle, "I2C_SCL_DIONUM", 0)
-
-    ljm.eWriteName(handle, "FIO2", 1)
+ljm.eWriteName(handle, "FIO2", 1) #using FIO2 as power, setting to high continously
 
 ljm.eWriteName(handle, "I2C_SPEED_THROTTLE", 65000)
 
-ljm.eWriteName(handle,"IWC_SLAVE_ADDRESS", 0x44) #tentative address, will change after finding data sheet
+ljm.eWriteName(handle,"IWC_SLAVE_ADDRESS", 0x6B)
+
+#start signal
+ljm.eWriteName(handle, "FIO0", 1) #hold SCL HIGH
+ljm.eWriteName(handle, "FIO1", 1)
+ljm.eWriteName(handle, "FIO1", 0) #HIGH to LOW on the SDA
+#the I2C bus is now considered busy
 
 #tentative bytes to transmit and recieve
-ljm.eWriteName(handle, "I2C_NUM_BYTES_TX", 2)
+ljm.eWriteName(handle, "I2C_NUM_BYTES_TX", 1)
 ljm.eWriteName(handle, "I2C_NUM_BYTES_RX", 0)
 
-aBytes = [0x24, 0x00] #[clock stretching disabled, high repeatability] 
-                      #Will need to look at data sheet to determine best use of options
+ljm.eWriteName(handle, "I2C_DATA_TX", 0xD5) #send address of the slave (1101010b or 0x6B) followed by 1 for a read transaction (11010101b or 0xD5)
 
-ljm.eWriteName(handle, "I2C_GO", 1) #Begin I2C communications
+ljm.eWriteName(handle, "I2C_GO", 1) #send data and begin 
 
-    
