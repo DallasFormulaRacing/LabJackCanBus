4c4da042954b6e144dea872f23e56a7d52ead962
NicoOhR <nimoohayonr@gmail.com>
2024-01-19 15:10:58 -0600

    output, no ack

--- accel.py
+++ accel.py
@@ -9,7 +9,6 @@ TODO:
 """
 
 from labjack import ljm
-from labjack import I2C
 
 handle = ljm.openS("ANY","ANY","ANY")
 
@@ -20,51 +19,25 @@ ljm.eWriteName(handle, "I2C_SCL_DIONUM", 0) #using FIO0 as SCL
 
 ljm.eWriteName(handle, "FIO2", 1) #using FIO2 as power, setting to high continously
 
-ljm.eWriteName(handle, "I2C_SPEED_THROTTLE", 65000)
+ljm.eWriteName(handle, "I2C_SPEED_THROTTLE", 50000)
 
-ljm.eWriteName(handle,"IWC_SLAVE_ADDRESS", 0x19)
+ljm.eWriteName(handle,"I2C_SLAVE_ADDRESS", 0x19)
 
-#start signal
-ljm.eWriteName(handle, "FIO0", 1) #hold SCL HIGH
-ljm.eWriteName(handle, "FIO1", 1)
-ljm.eWriteName(handle, "FIO1", 0) #HIGH to LOW on the SDA
-#the I2C bus is now considered busy
+ljm.eWriteName(handle,"I2C_OPTIONS", 0)
 
-#tentative bytes to transmit and recieve
 ljm.eWriteName(handle, "I2C_NUM_BYTES_TX", 1)
 ljm.eWriteName(handle, "I2C_NUM_BYTES_RX", 0)
 
-ljm.eWriteName(handle, "I2C_DATA_TX", 0x33) #send address of the slave (0011001b or 0x16) followed by 1 for a write command (0011001110b or 0x32)
+ljm.eWriteNameByteArray(handle, "I2C_DATA_TX", 1, [0x31])
 
 ljm.eWriteName(handle, "I2C_GO", 1) #send data and begin 
 
 ljm.eWriteName(handle, "I2C_NUM_BYTES_TX", 0)
-ljm.eWriteName(handle, "I2C_NUM_BYTES_RX", 1)
+ljm.eWriteName(handle, "I2C_NUM_BYTES_RX", 6)
 
-ljm.eWriteName(handle, "I2C_GO", 1) #Read Slave Aknowledgement (SAK)
+ljm.eWriteName(handle, "I2C_GO", 1) 
 
-SAK = ljm.eReadNameByteArray(handle, "I2C_DATA_RX", 1)
+DATA = ljm.eReadNameByteArray(handle, "I2C_DATA_RX", 12)
 
+SAK = ljm.eReadNameByteArray(handle, "I2C_ACKS", 2);
 print(SAK)
-
-ljm.eWriteName(handle, "I2C_NUM_BYTES_TX", 1)
-ljm.eWriteName(handle, "I2C_NUM_BYTES_RX", 0)
-
-#place holder hex address
-ljm.eWriteName(handle,"I2C_DATA_TX", 0x99) #SUB address format is 1 followed by the address of the register you want to read
-
-ljm.eWriteName(handle, "I2C_GO", 1) #sensor is ready to send DATA
-
-sleep(0.02) #wait for collected values
-
-#to continously read data from the sensor, send a Master acknowledge
-ljm.eWriteName(handle, "I2C_DATA_TX", 0x20) #0x20 is the temperature register, testing purposes
-
-ljm.eWriteName(handle, "I2C_NUM_BYTES_TX", 1)
-ljm.eWriteName(handle, "I2C_NUM_BYTES_RX", 2) #all registers return 2 bytes of data
-
-ljm.eWriteName(handle, "I2C_GO", 1) # collect data
-
-tempData = ljm.eWriteNameByteArray(handle, "I2C_DATA_RX", 2)
-
-ljm.close(handle) #close communications when finished reading data
