31ff908880092e220777d02ddb5601772983f107
NicoOhR <nimoohayonr@gmail.com>
2024-01-19 20:08:20 -0600

    switched to dec for now, need pull ups

--- accel.py
+++ accel.py
@@ -14,21 +14,23 @@ handle = ljm.openS("ANY","ANY","ANY")
 
 info = ljm.getHandleInfo(handle)
 
+print(info)
+
 ljm.eWriteName(handle, "I2C_SDA_DIONUM", 1) #using FIO1 as SDA
 ljm.eWriteName(handle, "I2C_SCL_DIONUM", 0) #using FIO0 as SCL
 
 ljm.eWriteName(handle, "FIO2", 1) #using FIO2 as power, setting to high continously
 
-ljm.eWriteName(handle, "I2C_SPEED_THROTTLE", 50000)
+ljm.eWriteName(handle, "I2C_SPEED_THROTTLE", 65000)
 
-ljm.eWriteName(handle,"I2C_SLAVE_ADDRESS", 0x19)
+ljm.eWriteName(handle,"I2C_SLAVE_ADDRESS", 25)
 
 ljm.eWriteName(handle,"I2C_OPTIONS", 0)
 
 ljm.eWriteName(handle, "I2C_NUM_BYTES_TX", 1)
 ljm.eWriteName(handle, "I2C_NUM_BYTES_RX", 0)
 
-ljm.eWriteNameByteArray(handle, "I2C_DATA_TX", 1, [0x31])
+ljm.eWriteNameByteArray(handle, "I2C_DATA_TX", 1, [45])
 
 ljm.eWriteName(handle, "I2C_GO", 1) #send data and begin 
 
@@ -40,4 +42,5 @@ ljm.eWriteName(handle, "I2C_GO", 1)
 DATA = ljm.eReadNameByteArray(handle, "I2C_DATA_RX", 12)
 
 SAK = ljm.eReadNameByteArray(handle, "I2C_ACKS", 2);
-print(SAK)
+
+print(DATA)
