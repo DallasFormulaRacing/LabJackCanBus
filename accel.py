163f5d6c974c12f922e5ed6bfbf71d8f9b284844
NicoOhR <nimoohayonr@gmail.com>
2024-01-21 14:31:34 -0600

    reading from registers

--- accel.py
+++ accel.py
@@ -5,9 +5,10 @@ based on the sht3x.py example in the labjack documentation
 
 TODO: 
 *integrate into DAQ class
-*Look into more readable implementation using the I2C library from labjack
+*convert data to usable format
+*write data to CSV
 """
-
+import time
 from labjack import ljm
 
 handle = ljm.openS("ANY","ANY","ANY")
@@ -21,26 +22,30 @@ ljm.eWriteName(handle, "I2C_SCL_DIONUM", 0) #using FIO0 as SCL
 
 ljm.eWriteName(handle, "FIO2", 1) #using FIO2 as power, setting to high continously
 
-ljm.eWriteName(handle, "I2C_SPEED_THROTTLE", 65000)
+ljm.eWriteName(handle, "I2C_SPEED_THROTTLE", 65516)
 
-ljm.eWriteName(handle,"I2C_SLAVE_ADDRESS", 25)
+ljm.eWriteName(handle,"I2C_SLAVE_ADDRESS", 0x19)
 
 ljm.eWriteName(handle,"I2C_OPTIONS", 0)
 
-ljm.eWriteName(handle, "I2C_NUM_BYTES_TX", 1)
+ljm.eWriteName(handle, "I2C_NUM_BYTES_TX", 4)
 ljm.eWriteName(handle, "I2C_NUM_BYTES_RX", 0)
 
-ljm.eWriteNameByteArray(handle, "I2C_DATA_TX", 1, [45])
+ljm.eWriteNameByteArray(handle, "I2C_DATA_TX", 2, [0x20, 0x27]) #Data Rate is set to 10Hz, disable low power, enable all axies
+ljm.eWriteNameByteArray(handle, "I2C_DATA_TX", 2, [0x23, 0x49]) #Continous update
 
 ljm.eWriteName(handle, "I2C_GO", 1) #send data and begin 
 
-ljm.eWriteName(handle, "I2C_NUM_BYTES_TX", 0)
-ljm.eWriteName(handle, "I2C_NUM_BYTES_RX", 6)
+ljm.eWriteName(handle, "I2C_NUM_BYTES_TX", 1)
+ljm.eWriteName(handle, "I2C_NUM_BYTES_RX", 2)
 
-ljm.eWriteName(handle, "I2C_GO", 1) 
+while True:
+    ljm.eWriteNameByteArray(handle, "I2C_DATA_TX",1,[0x28])
+    DATA = ljm.eReadNameByteArray(handle, "I2C_DATA_RX", 2)
 
-DATA = ljm.eReadNameByteArray(handle, "I2C_DATA_RX", 12)
+    ljm.eWriteName(handle, "I2C_GO", 1) 
 
-SAK = ljm.eReadNameByteArray(handle, "I2C_ACKS", 2);
+    SAK = ljm.eReadNameByteArray(handle, "I2C_ACKS", 2);
 
-print(DATA)
+    print(DATA)
+    time.sleep(1)
