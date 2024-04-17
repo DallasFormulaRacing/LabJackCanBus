14bb3e1c4e3ed2fbeb55b7eaa857e5e0fe3ad35a
GHAFHA <45609663+GHAFHA@users.noreply.github.com>
2024-04-17 15:31:35 -0500

    Refactor sensor classes and add telemetry metrics

--- sensors/GyroAndAccel.py
+++ sensors/GyroAndAccel.py
@@ -11,12 +11,12 @@ import json
 
 from telegraf.client import TelegrafClient
 
-
-TIME_ID = "t"
+telegraf_client = TelegrafClient(host="localhost", port=8092)
 
 
 class Accelerometer(Extension):
-    def __init__(self):
+    def __init__(self, session_id: int):
+        self.session_id = session_id
         self.i2c = None
         self.accelerometer = None
         self.setup()
@@ -27,20 +27,23 @@ class Accelerometer(Extension):
 
     def poll(self) -> pd.DataFrame:
         accel_x, accel_y, accel_z = self.accelerometer.acceleration
-        t = time.time()
+        timestamp = time.time()
 
-        return pd.DataFrame(
-            data={
-                "t": t,
-                "x": [accel_x],
-                "y": [accel_y],
-                "z": [accel_z]
-            }
-        )
+        payload = {
+            "t": timestamp,
+            "x": [accel_x],
+            "y": [accel_y],
+            "z": [accel_z]
+        }
+
+        telegraf_client.metric("accel_values", payload, tags={"source": "accel", "session_id": self.session_id})
+
+        return pd.DataFrame(data=payload)
 
 
 class Gyro(Extension):
-    def __init__(self):
+    def __init__(self, session_id: int):
+        self.session_id = session_id
         self.i2c = None
         self.gyroscope = None
         self.setup()
@@ -51,16 +54,18 @@ class Gyro(Extension):
 
     def poll(self) -> pd.DataFrame:
         ang_x, ang_y, ang_z = self.gyroscope.gyro
-        t = time.time()
+        timestamp = time.time()
 
-        return pd.DataFrame(
-            data={
-                "t": t,
-                "angular x": [ang_x],
-                "angular y": [ang_y],
-                "angular z": [ang_z]
-            }
-        )
+        payload = {
+            "t": timestamp,
+            "angular x": [ang_x],
+            "angular y": [ang_y],
+            "angular z": [ang_z]
+        }
+
+        telegraf_client.metric("gyro_values", payload, tags={"source": "gyro", "session_id": self.session_id})
+
+        return pd.DataFrame(data=payload)
 
 
 class Read(Thread):
@@ -74,58 +79,28 @@ class Read(Thread):
         self.gyro_df = pd.DataFrame()
         self.telegraf_client = TelegrafClient(host="localhost", port=8092)
         self.session_id = self.retrieve_session_id()
-
-    def process(self):
-        # intiialzize
-        gyro = Gyro()
-        xl = Accelerometer()
-
-        # loop
-        while not self._stop.is_set():
-            gyro_data = gyro.poll()
-            xl_data = xl.poll()
-            pd.concat([self.accel_df, xl_data], ignore_index=True)
-            pd.concat([self.gyro_df, gyro_data], ignore_index=True)
+        self.gyro = Gyro(self.session_id)
+        self.accel = Accelerometer(self.session_id)
 
     def retrieve_session_id(self):
         with open("config.json", "r") as config_file:
             config = json.load(config_file)
-            return config["session_id"]
+            session_id = config["session_id"]
+            return session_id
 
-    def send_accel_metrics(self, accel_df: pd.DataFrame):
-        for i, row in accel_df.iterrows():
-            time_value = row[TIME_ID]
-            payload = {
-                'fields': {
-                    'accel_x': row['x'],
-                    'accel_y': row['y'],
-                    'accel_z': row['z']
-                }
-            }
-            self.telegraf_client.metric("accel_values", payload, tags={"source": "accel", "session_id": self.session_id}, timestamp=time_value)
-
-    def send_gyro_metrics(self, gyro_df: pd.DataFrame):
-        for i, row in gyro_df.iterrows():
-            time_value = row[TIME_ID]
-
-            payload = {
-                'fields': {
-                    'angular_x': row['angular x'],
-                    'angular_y': row['angular y'],
-                    'angular_z': row['angular z']
-                }
-            }
-            self.telegraf_client.metric("gyro_values", payload, tags={"source": "gyro", "session_id": self.session_id}, timestamp=time_value)
+    def process(self):
+        # loop
+        while not self._stop.is_set():
+            gyro_data = self.gyro.poll()
+            xl_data = self.accel.poll()
+            pd.concat([self.accel_df, xl_data], ignore_index=True)
+            pd.concat([self.gyro_df, gyro_data], ignore_index=True)
 
     def stop(self):
         self._stop.set()
         self.join()
 
     def save(self, accel_fp: str, gyro_fp: str):
-
-        self.send_accel_metrics(self.accel_df)
-        self.send_gyro_metrics(self.gyro_df)
-
         self.accel_df.to_csv(accel_fp, index=False)
         self.gyro_df.to_csv(gyro_fp, index=False)
 
