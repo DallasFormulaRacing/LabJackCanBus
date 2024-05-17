f9114506a574bf4897cf27bd2b664c6233976f87
Sahan Wick <syw210001@utdallas.edu>
2024-05-17 01:39:14 -0500

    used csvwriter library instead of manually writing

--- telegraf_csv_outputter.py
+++ telegraf_csv_outputter.py
@@ -1,4 +1,5 @@
 from line_protocol_parser import parse_line
+import csv
 import os
 
 # Output folder path
@@ -9,7 +10,7 @@ sensor_files = {}
 
 while True:
     data = input()
-    
+
     try:
         line_data = parse_line(data)
     except:
@@ -21,31 +22,31 @@ while True:
 
     print(line_data)
     session_id = line_data["tags"]["session_id"]
-    
-    # session_folder = os.path.join(OUTPUT_FOLDER_PATH, rf"session_{session_id}")
-    # if not os.path.exists(session_folder):
-    #     os.makedirs(session_folder)
-    
     sensor_type = line_data["tags"]["source"]
-    sensor_file_path = os.path.join(OUTPUT_FOLDER_PATH, f"{session_id}-{sensor_type}.csv")
+    sensor_file_path = os.path.join(
+        OUTPUT_FOLDER_PATH, f"{session_id}-{sensor_type}.csv"
+    )
+
+    file_changed = True
 
     if sensor_type not in sensor_files:
-        sensor_files[sensor_type] = open(
-            sensor_file_path, "a"
-        )
-        sensor_files[sensor_type].write(
-            "time," + ",".join(line_data["fields"].keys()) + "\n"
-        )
+        sensor_files[sensor_type] = open(sensor_file_path, "a")
 
     # Different session id
     elif sensor_files[sensor_type].name != sensor_file_path:
         sensor_files[sensor_type].close()
-        sensor_files[sensor_type] = open(
-            sensor_file_path, "a"
-        )
+        sensor_files[sensor_type] = open(sensor_file_path, "a")
 
-    sensor_files[sensor_type].write(
-        f"{line_data['time']},"
-        + ",".join([str(value) for value in line_data["fields"].values()])
-        + "\n"
+    else:
+        file_changed = False
+
+    fields = ["time"] + list(line_data["fields"].keys())
+    writer = csv.writer(
+        sensor_files[sensor_type], delimiter=",", lineterminator="\n", strict=True
     )
+
+    if file_changed and os.stat(sensor_file_path).st_size == 0:
+        writer.writerow(fields)
+
+    else:
+        writer.writerow([line_data["time"]] + list(line_data["fields"].values()))
